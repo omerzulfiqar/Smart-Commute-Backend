65e5d478a0176f944a0bfbe6b7b1662da5f2a036
tsu <hypersonic5421@gmail.com>
2019-11-10 22:48:35 -0500

    update env

--- config/env.js
+++ config/env.js
@@ -1,12 +1,11 @@
 
 const env = process.env.NODE_ENV;
-const local = require('./local.js');
 
 let config = {};
 if (env === 'HEROKU') {
   config.mongodbUrl = process.env.mongodbUrl;
 } else {
-  config = local;
+  config = require('./local.js');
 }
 
 module.exports = config;
