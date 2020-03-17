7c7f8285a3317471c4511933d46eb3944c0ade0f
tsu <hypersonic5421@gmail.com>
2020-03-17 13:43:53 -0400

    update config

--- config/env.js
+++ config/env.js
@@ -2,10 +2,11 @@
 const env = process.env.NODE_ENV;
 
 let config = {};
-if (env === 'HEROKU') {
-  config.mongodbUrl = process.env.mongodbUrl;
-} else {
-  config = require('./local.js');
-}
+config.mongodbUrl = "mongodb+srv://cs5614:PKVrJTfnwxKW6o3b@cs5614-ckoux.gcp.mongodb.net/test?retryWrites=true&w=majority"
+// if (env === 'HEROKU') {
+//   config.mongodbUrl = process.env.mongodbUrl;
+// } else {
+//   config = require('./local.js');
+// }
 
 module.exports = config;
