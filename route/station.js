5a90105304b5fb679aba37fbd2936f62a1cda4a4
tsu <hypersonic5421@gmail.com>
2020-03-18 16:53:22 -0400

    update data sort by date

--- route/station.js
+++ route/station.js
@@ -8,25 +8,25 @@ const { removeEntry, addEntry } = require('../model/event.js');
 const router = express.Router();
 
 router.get('/', async (_req, res) => {
-    try {
-        const data = require('../model/station');
-        const events = await db.db('twitter').collection('threatevent').find({ 
-        }).sort({ datefield: 1 })
-        .toArray();
-        _.each(events, event => {
-            const station = _.find(data.Stations, {'Code': event.code});
-            if(!_.isEmpty(event.code) && !_.isNil(station)) {
-                station.stories.push(event);
-            } 
-        });
-        events.sort(function compare(a, b) {
-            var dateA = new Date(a.date);
-            var dateB = new Date(b.date);
-            return dateA - dateB;
-        });
-        res.status(HTTPStatus.OK).json(data);
-    } catch (err) {
-        res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json(err);
-    }
+  try {
+    const data = await db.db('twitter').collection('station').find({ 
+    }).toArray();
+    const events = await db.db('twitter').collection('threatevent').find({ 
+    }).toArray();
+    _.each(events, event => {
+      const station = _.find(data[0].Stations, {'Code': event.code});
+      if (!_.isEmpty(event.code) && !_.isNil(station)) {
+        station.stories.push(event);
+      }
+      station.stories = _.sortBy(station.stories, ['date']);
+    });
+    _.each(data[0].Stations, station => {
+      station.stories = _.sortBy(station.stories, ['date']);
+    });
+
+    res.status(HTTPStatus.OK).json(data[0]);
+  } catch (err) {
+    res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json(err);
+  }
 });
 module.exports = router;
